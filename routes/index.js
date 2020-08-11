93abcb4c1764dc865adc349eea08c6dc71a1dd73
GamJarin <59593043+JangIBin@users.noreply.github.com>
2020-08-11 14:54:23 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var router = express.Router();
+var cors = require("cors");
+var Problem = require("../models/Problem");
+var vm = require("vm");
+
+// RESTful
+router.options("/problems/:problem_id", cors());
+
+router.post("/problems/:problem_id", cors(), async function(req, res, next) {
+  //console.log(req.param.problem_id)
+  /* 
+  1. 클라이언트가 전달해준 사용자의 문제 풀이 내용 -> 실행
+  2. 실행한 결과 === DB에 저장되어 있는 해당 문제의 정답과 일치하는지 비교
+  3. 비교 결과를 클라이언트에 응답
+  */
+
+  const body = req.body;
+
+  //사용자가 풀이한 내용
+  //Client: "function solution (n) {return x * 3;}"
+  const code = body.code; 
+  
+  try {
+    const problem = await Problem.findById(req.params.problem_id);
+
+    let isCorrect = true;
+
+    for (let i=0;i < problem.tests.length;i++) {
+      try {
+        const script = new vm.Script(code + problem.tests[i].code);
+        const result = script.runInNewContext();
+  
+        if(`${result}` !== problem.tests[i].solution) {
+          isCorrect = false;
+        }
+      } catch (err) {
+        res.json({
+          result: "에러",
+          detail: err.message
+        });
+      }
+    }
+
+    //Status Code: 200 Success` 
+    if (!isCorrect) {
+      res.json({ result:" 오답 " });
+    } else {
+      res.json({ result:" 정답 " });
+    }
+
+  } catch (err) {
+    next(err)
+  }
+});
+
+/* GET home page. */
+router.get("/problems", cors(), async function(req, res, next) {
+  try {
+    const docs = await Problem.find();
+    res.json(docs);
+  } catch (err) {
+    //err handing
+    next(err);
+  }
+  
+});
+
+module.exports = router;
